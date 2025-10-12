8f0bc34d20680822bceda194109c161a2c4f26c8
Norrheden <eliasnorrhedenmau@gmail.com>
2025-10-12 16:55:14 +0200

    Add egg timer selection and list functionality
    Introduces logic for selecting egg size and boiling preference, calculates corresponding boiling times, and allows adding egg configurations to a list. Updates HTML to support new elements and event handling for adding eggs to the list.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let listPageButton = document.getElementById("listPageButton")
-let homePage = document.getElementById("homePage") 
-let listPage = document.getElementById("listPage")
-let homePageButton = document.getElementById("homePageButton")
-
-
-
-
+let listPageButton = document.getElementById("listPageButton");
+let homePage = document.getElementById("homePage");
+let listPage = document.getElementById("listPage");
+let homePageButton = document.getElementById("homePageButton");
+let addList = document.getElementById("addList");
+let time = document.getElementById("time");
+let container = document.getElementById("container")
 //Home and list page
 listPageButton.addEventListener("click", function () {
     homePage.style.display = "none";
@@ -16,3 +15,93 @@ homePageButton.addEventListener("click", function() {
     listPage.style.display = "none"
 })
 
+/////////////////////////////////////
+let eggArray = [];
+let sizeForEgg;
+let boilningForEgg;
+
+
+//Event listener for radio size button
+let radioButtonSize = document.querySelectorAll("input[name = 'size']")
+for(let i = 0; i<radioButtonSize.length; i++) {
+    radioButtonSize[i].addEventListener("click", function() {
+        console.log(radioButtonSize[i].value)
+        sizeForEgg = radioButtonSize[i].value;
+        calcTimeEgg()
+
+
+    }) 
+}
+//Event listener for radio boilning button
+let radioButtonsBoilning = document.querySelectorAll("input[name='boilning']")
+for(let i = 0; i<radioButtonsBoilning.length; i++) {
+    radioButtonsBoilning[i].addEventListener("click", function() {
+        console.log(radioButtonsBoilning[i].value);
+        boilningForEgg = radioButtonsBoilning[i].value;
+        calcTimeEgg()
+    })
+}
+
+//Add to egg array
+addList.addEventListener("click", function() {
+    let newEgg = {
+        size: sizeForEgg,
+        boilning: boilningForEgg,
+        time: calcTimeEgg()
+    }
+    eggArray.push(newEgg)
+    console.log(eggArray)
+    addToContainer()
+})
+
+
+
+
+
+function calcTimeEgg() {
+    //Source ica 
+    if(sizeForEgg === "small" && boilningForEgg === "soft") {
+        time.textContent = "06:00"
+        return "06:00"
+    }
+    if(sizeForEgg === "small" && boilningForEgg === "medium") {
+        time.textContent = "08:00"
+        return "08:00"
+    }
+    if(sizeForEgg === "small" && boilningForEgg === "hard") {
+        time.textContent = "10:00"
+        return "10:00"
+    }
+
+    if(sizeForEgg === "medium" && boilningForEgg === "soft") {
+        time.textContent = "07:00"
+        return "07:00"
+    }
+    if(sizeForEgg === "medium" && boilningForEgg === "medium") {
+        time.textContent = "09:00"
+        return "09:00"
+    }
+    if(sizeForEgg === "medium" && boilningForEgg === "hard") {
+        time.textContent = "11:00"
+        return "11:00"
+    }
+
+    if(sizeForEgg === "large" && boilningForEgg === "soft") {
+        time.textContent = "08:00"
+        return "08:00"
+    }
+    if(sizeForEgg === "large" && boilningForEgg === "medium") {
+        time.textContent = "10:00"
+        return "10:00"
+    }
+    if(sizeForEgg === "large" && boilningForEgg === "hard") {
+        time.textContent = "12:00"
+        return "12:00"
+    }
+}
+
+function addToContainer() {
+    for(let egg of eggArray) {
+        
+    }
+}
